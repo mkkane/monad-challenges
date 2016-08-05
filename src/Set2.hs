8f72f900792ca9c1046669a349565d628118e3a8
Michael Kane <m.k.kane@gmail.com>
2016-08-05 17:59:40 +0100

    queryGreek2

--- src/Set2.hs
+++ src/Set2.hs
@@ -62,6 +62,20 @@ queryGreek d s = case lookupMay s d of
         Nothing -> Nothing
         Just headOfXs -> divMay (fromIntegral maximumOfTailOfXs) (fromIntegral headOfXs)
 
+testQueryGreek :: (GreekData -> String -> Maybe Double) -> Bool
+testQueryGreek queryGreekFn = all (== True)
+  [ queryGreekFn greekDataA "alpha" == Just 2.0
+  , queryGreekFn greekDataA "beta" == Nothing
+  , queryGreekFn greekDataA "gamma" == Just 3.3333333333333335
+  , queryGreekFn greekDataA "delta" == Nothing
+  , queryGreekFn greekDataA "zeta" == Nothing
+  , queryGreekFn greekDataB "rho" == Nothing
+  , queryGreekFn greekDataB "phi" == Just 0.24528301886792453
+  , queryGreekFn greekDataB "chi" == Just 9.095238095238095
+  , queryGreekFn greekDataB "psi" == Nothing
+  , queryGreekFn greekDataB "omega" == Just 24.0
+  ]
+
 chain :: (a -> Maybe b) -> Maybe a -> Maybe b
 chain _ Nothing = Nothing
 chain f (Just a) = f a
@@ -69,10 +83,10 @@ chain f (Just a) = f a
 link :: Maybe a -> (a -> Maybe b) -> Maybe b
 link = flip chain
 
---queryGreek2 :: GreekData -> String -> Maybe Double
---queryGreek2 d s = (\x -> \y -> divMay (fromIntegral x) (fromIntegral y)) (chain headMay $ lookupMay s d) (chain maximumMay $ chain tailMay $ lookupMay s d)
-
--- div $ (fromIntegral . maximum . tail . lookup) s d (fromIntegral . head . lookup) s d
-
 queryGreek2 :: GreekData -> String -> Maybe Double
-queryGreek2 = undefined
+queryGreek2 = \d s -> link (lookupMay s d)
+  $ \xs -> link (tailMay xs)
+  $ \tailOfXs -> link (maximumMay tailOfXs)
+  $ \maximumOfTailOfXs -> link (headMay xs)
+  $ \headOfXs -> divMay (fromIntegral maximumOfTailOfXs) (fromIntegral headOfXs)
+
