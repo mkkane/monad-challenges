6db8034084031b4e5d6e801680d48699561f2e73
Michael Kane <m.k.kane@gmail.com>
2016-08-07 19:18:09 +0100

    Set4 wipping

--- src/Set4.hs
+++ src/Set4.hs
@@ -4,8 +4,9 @@
 module Set4 where
 
 import MCPrelude
-import Set1 (hexDecode, Gen, genTwo, mkGen, generalA)
-import Set2 (Maybe, link, mkMaybe)
+import Set1 (hexDecode)
+-- import Set1 (hexDecode, Gen, genTwo, mkGen, generalA)
+import Set2
 
 -- Set1 general fns:
 --
@@ -32,20 +33,53 @@ genBYLink = undefined
 genTwoLink :: m a -> (a -> m b) -> m b
 genTwoLink = undefined
 
-generalB2 :: (a -> b -> c) -> Gen a -> Gen b -> Gen c
-generalB2 f ga gb = genTwo ga
-  $ \a -> genTwo gb
-  $ \b -> mkGen (f a b)
+-- generalB2 :: (a -> b -> c) -> Gen a -> Gen b -> Gen c
+-- generalB2 f ga gb = genTwo ga
+--   $ \a -> genTwo gb
+--   $ \b -> mkGen (f a b)
 
-yLink2 :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
-yLink2 f ma mb = link ma
-  $ \a -> link mb
-  $ \b -> mkMaybe (f a b)
+-- yLink2 :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+-- yLink2 f ma mb = link ma
+--   $ \a -> link mb
+--   $ \b -> mkMaybe (f a b)
 
-generalA2 :: (a -> b) -> Gen a -> Gen b
-generalA2 f ga = genTwo ga (mkGen . f)
+-- generalA2 :: (a -> b) -> Gen a -> Gen b
+-- generalA2 f ga = genTwo ga (mkGen . f)
 
-repRandom2 :: [Gen a] -> Gen [a]
-repRandom2 [] = mkGen []
-repRandom2 (ga:gas) = genTwo ga (\a -> generalA2 (a:) (repRandom2 gas))
+-- repRandom2 :: [Gen a] -> Gen [a]
+-- repRandom2 [] = mkGen []
+-- repRandom2 (ga:gas) = genTwo ga (\a -> generalA2 (a:) (repRandom2 gas))
 
+class Monad m where
+  bind :: m a -> (a -> m b) -> m b
+  return :: a -> m a
+
+generalB2YLink :: (Monad m) => (a -> b -> c) -> m a -> m b -> m c
+generalB2YLink f ma mb = bind ma
+  $ \a -> bind mb
+  $ \b -> return (f a b)
+
+instance Monad Maybe where
+  bind Nothing _ = Nothing
+  bind (Just a) f = f a
+  return = Just
+
+instance Monad [] where
+  bind [] _ = []
+  bind (a:as) f = f a ++ bind as f
+  return a = [a]
+
+-- newtype Gen a = Gen (Seed -> (a, Seed))
+
+-- instance Monad Gen where
+--   bind (Gen ga) f = Gen (\s0 -> (let (a, sa) = ga s0 in (let Gen gb = f a in gb sa)))
+--   return a = Gen ((,) a) -- Gen (\s -> (a, s))
+
+newtype Gen a = Gen { runGen :: Seed -> (a, Seed) }
+
+instance Monad Gen where
+  bind ga f = Gen (\s0 -> let (a, sa) = runGen ga s0 in runGen (f a) sa)
+  return a = Gen ((,) a)
+
+evalGen :: Gen a -> Seed -> a
+evalGen (Gen ga) = fst . ga
