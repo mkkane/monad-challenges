a939a5999ef86b3f077957150fbe6d46d38a4aaf
Michael Kane <m.k.kane@gmail.com>
2016-08-08 14:28:05 +0100

    Set4 done

--- src/Set4.hs
+++ src/Set4.hs
@@ -5,8 +5,14 @@ module Set4 where
 
 import MCPrelude
 import Set1 (hexDecode)
--- import Set1 (hexDecode, Gen, genTwo, mkGen, generalA)
-import Set2
+import Set2 ( Maybe(..)
+            , headMay
+            , tailMay
+            , lookupMay
+            , divMay
+            , maximumMay
+            , minimumMay )
+import Set3 (Card(..))
 
 -- Set1 general fns:
 --
@@ -106,3 +112,125 @@ liftM3 f ma mb mc = bind ma (\a -> liftM2 (f a) mb mc)
 
 ap :: (Monad m) => m (a -> b) -> m a -> m b
 ap = liftM2 (\f -> \a -> f a)
+
+-- Kinda useful to have this too...
+liftM :: (Monad m) => (a -> b) -> m a -> m b
+liftM f = (=<<) (return . f)
+
+-- ===================================================================
+-- Set1
+-- ===================================================================
+
+randInt :: Gen Integer
+randInt = Gen rand
+
+fiveRands :: [Integer]
+fiveRands = evalGen (sequence (replicate 5 (randInt))) (mkSeed 1)
+
+randLetter :: Gen Char
+randLetter = liftM toLetter randInt
+
+randString3 :: String
+randString3 = evalGen (sequence (replicate 3 randLetter)) (mkSeed 1)
+
+generalA :: (a -> b) -> Gen a -> Gen b
+generalA = liftM
+
+randEven :: Gen Integer
+randEven = liftM (* 2) randInt
+
+randOdd :: Gen Integer
+randOdd = liftM (+ 1) randEven
+
+randTen :: Gen Integer
+randTen = liftM (* 10) randInt
+
+randPair :: Gen (Char, Integer)
+randPair = bind randLetter
+  $ \c -> bind randInt
+  $ \i -> return (c, i)
+  -- (better...) liftM2 (,) randLetter randInt
+
+generalPair :: Gen a -> Gen b -> Gen (a, b)
+generalPair = liftM2 (,)
+
+generalB :: (a -> b -> c) -> Gen a -> Gen b -> Gen c
+generalB = liftM2
+
+repRandom :: [Gen a] -> Gen [a]
+repRandom = sequence
+
+genTwo :: Gen a -> (a -> Gen b) -> Gen b
+genTwo = bind
+
+mkGen :: a -> Gen a
+mkGen = return
+
+-- ===================================================================
+-- Set2
+-- ===================================================================
+
+queryGreek :: GreekData -> String -> Maybe Double
+queryGreek d s = bind (lookupMay s d)
+  $ \xs -> bind (tailMay xs)
+  $ \tailOfXs -> bind (maximumMay tailOfXs)
+  $ \maximumOfTailOfXs -> bind (headMay xs)
+  $ \headOfXs -> divMay (fromIntegral maximumOfTailOfXs) (fromIntegral headOfXs)
+
+chain :: (a -> Maybe b) -> Maybe a -> Maybe b
+chain = (=<<)
+
+link :: Maybe a -> (a -> Maybe b) -> Maybe b
+link = bind
+
+addSalaries :: [(String, Integer)] -> String -> String -> Maybe Integer
+addSalaries t k1 k2 = liftM2 (+) (lookupMay k1 t) (lookupMay k2 t)
+
+yLink :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+yLink = liftM2
+
+mkMaybe :: a -> Maybe a
+mkMaybe = return
+
+transMaybe :: (a -> b) -> Maybe a -> Maybe b
+transMaybe = liftM
+
+tailProd :: Num a => [a] -> Maybe a
+tailProd = (liftM product) . tailMay
+
+tailSum :: Num a => [a] -> Maybe a
+tailSum = (liftM sum) . tailMay
+
+tailMax :: Ord a => [a] -> Maybe (Maybe a)
+tailMax = (liftM maximumMay) . tailMay
+
+tailMin :: Ord a => [a] -> Maybe (Maybe a)
+tailMin = (liftM minimumMay) . tailMay
+
+combine :: Maybe (Maybe a) -> Maybe a
+combine = join
+
+tailMaxCombined :: Ord a => [a] -> Maybe a
+tailMaxCombined = join . tailMax
+
+tailMinCombined :: Ord a => [a] -> Maybe a
+tailMinCombined = join . tailMin
+
+-- ===================================================================
+-- Set3
+-- ===================================================================
+
+allPairs :: [a] -> [b] -> [(a, b)]
+allPairs = liftM2 (,)
+
+allCards :: [Int] -> [String] -> [Card]
+allCards = liftM2 Card
+
+combStep :: [a -> b] -> [a] -> [b]
+combStep = ap
+
+allCombs :: (a -> b -> c) -> [a] -> [b] -> [c]
+allCombs = liftM2
+
+allCombs3 :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+allCombs3 = liftM3
